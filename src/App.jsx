7d7b999c7847c74f240d53746ba480fb21cc4538
Harsh <harshlad@492002@gmail.com>
2025-07-01 10:38:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import './App.css'
+import React from 'react'
+function App() {
+  const [data,setData] = React.useState("")
+    const clicked= async()=>{
+      const data = prompt("Enter you name");
+      let res=await axios.get(`http://localhost:5000/${data}`)
+      setData(res.data)
+      console.log(res.data)
+    }
+ 
+  return (
+    <>
+    <div style={{ height:"100vh", width:"100vw", background:data }} > <button  onClick={clicked}>Clicked</button></div>
+   
+    </>
+  )
+}
+
+export default App
